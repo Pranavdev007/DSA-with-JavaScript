f84092c65b175ddc126550d28c9643163bf32a43
Pranavdev007 <pranavdev1999@gmail.com>
2025-03-17 23:59:42 +0530

    Update 3-hamming-distance.js

--- Strings/3-hamming-distance.js
+++ Strings/3-hamming-distance.js
@@ -8,22 +8,6 @@
 //   (hwllr)
 //     ↑  ↑
 
-// function hammingDistance(x, y) {
-//   if (x.length !== y.length) {
-//     throw new Error("Strings must be of same length");
-//   }
-
-//   let distance = 0;
-
-//   for (let i = 0; i < x.length; i++) {
-//     if (x[i] !== y[i]) {
-//       distance++;
-//     }
-//   }
-
-//   return distance;
-// }
-
 // console.log(hammingDistance("hello", "hwllr"));
 
 // Variation 2: Given two integers x and y, return the Hamming distance between thier bits.
