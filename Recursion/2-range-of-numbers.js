d83d34279b282b4d9524bfea64bb84bb8b6c8623
Pranavdev007 <pranavdev1999@gmail.com>
2025-03-27 22:07:59 +0530

    Update 2-range-of-numbers.js

--- Recursion/2-range-of-numbers.js
+++ Recursion/2-range-of-numbers.js
@@ -1,15 +1,15 @@
 // Ques 2 : Create an array with range of numbers
 // Input:  start=1, end=5  ----->>>>>  Output:
 
-// function rangeOfNumbers(startNum, endNum) {
-//   if (endNum < startNum) {
-//     return [];
-//   } else {
-//     const numbers = rangeOfNumbers(startNum, endNum - 1);
-//     numbers.push(endNum);
-//     return numbers;
-//   }
-// }
+function rangeOfNumbers(startNum, endNum) {
+  if (endNum < startNum) {
+    return [];
+  } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
 
 // rangeOfNumbers(1,5) => [1,2,3,4,5]
 // rangeOfNumbers(1,4) => [1,2,3,4]
@@ -18,4 +18,4 @@
 // rangeOfNumbers(1,1) => [1]
 // rangeOfNumbers(1,0) => []
 
-// console.log(rangeOfNumbers(0, 5));
+console.log(rangeOfNumbers(0, 5));
