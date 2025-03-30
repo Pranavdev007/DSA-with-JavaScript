5ca8ec33e131897972fe9c49e717bb4ccbc5e747
Pranavdev007 <pranavdev1999@gmail.com>
2025-03-30 21:54:23 +0530

    Update Best-Time-to-Buy-and-Sell-Stock.js

--- Arrays/Best-Time-to-Buy-and-Sell-Stock.js
+++ Arrays/Best-Time-to-Buy-and-Sell-Stock.js
@@ -1,3 +1,11 @@
+// Question - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+// Example 1:
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
 function maxProfit(arr) {
   let maxProfit = 0;
   let currStock = arr[0];
