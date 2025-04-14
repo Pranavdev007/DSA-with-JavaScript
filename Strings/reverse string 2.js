5407d72bc6ca4707f4b3bd5f10e32e1bd639b143
Pranavdev007 <pranavdev1999@gmail.com>
2025-04-14 14:21:58 +0530

    Update reverse string 2.js

--- Strings/reverse string 2.js
+++ Strings/reverse string 2.js
@@ -10,3 +10,29 @@
 
 // Input: s = "abcd", k = 2
 // Output: "bacd"
+
+var reverseStr = function (s, k) {
+  // Convert the input string to an array to allow in-place modification
+  s = s.split("");
+  let n = s.length;
+
+  // Iterate over the string in chunks of 2k
+  for (let i = 0; i < n; i += 2 * k) {
+    // Set the start index of the portion to reverse
+    let start = i;
+
+    // Set the end index: reverse at most k characters, but don’t go past the end of the string
+    let end = Math.min(n - 1, i + k - 1);
+
+    // Reverse characters between start and end indices
+    while (start < end) {
+      // Swap the characters at start and end
+      [s[start], s[end]] = [s[end], s[start]];
+      start++;
+      end--;
+    }
+  }
+  // Convert the array back into a string and return it
+  return s.join("");
+};
+console.log(reverseStr("abcdefg"));
