9704768ee3eb5a29f53d9c98e0e5df69385345ae
Pranavdev007 <pranavdev1999@gmail.com>
2025-04-17 14:14:40 +0530

    Create longestCommonPrefix.js

---
+++ Strings/longestCommonPrefix.js
@@ -0,0 +1,23 @@
+function longestCommonPrefix(strs) {
+  // If the array is empty, return an empty string
+  if (!strs.length) return "";
+
+  // Assume the first string is the longest common prefix
+  let prefix = strs[0];
+
+  // Loop through the remaining strings
+  for (let i = 1; i < strs.length; i++) {
+    // While the current string doesn't start with the prefix (using indexOf)
+    while (strs[i].indexOf(prefix) !== 0) {
+      // Remove the last character from the prefix
+      prefix = prefix.slice(0, -1);
+
+      // If prefix becomes empty, there's no common prefix
+      if (!prefix) return "";
+    }
+  }
+
+  // Return the longest common prefix found
+  return prefix;
+}
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
