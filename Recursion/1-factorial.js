b231c587e2b656a6cb9ac7a64e5dfe6c7208eeb9
Pranavdev007 <pranavdev1999@gmail.com>
2025-04-20 23:59:47 +0530

    Update 1-factorial.js

--- Recursion/1-factorial.js
+++ Recursion/1-factorial.js
@@ -1,10 +1,10 @@
 // Ques 1 : Factorial of n
 // Input:  n = 5  ----->>>>>  Output: 120
 
-function factorial(n) {
+function Factorial(n) {
   if (n === 0) {
     return 1;
   } else return n * factorial(n - 1);
 }
 
-console.log(factorial(5));
+console.log(Factorial(5));
