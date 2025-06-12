69679659741cfa16f4cf1b68b8c30b709957e411
Pranavdev007 <pranavdev1999@gmail.com>
2025-06-12 23:29:49 +0530

    Update 4-maximum-sum-subarray.js

--- Arrays/4-maximum-sum-subarray.js
+++ Arrays/4-maximum-sum-subarray.js
@@ -5,28 +5,28 @@
 // Input: [5,4,-1,7,8]                ----->>>>>    Output: 23,  [5,4,-1,7,8]
 
 // Brute Force Approach
-function maxSubArrayBruteForce(nums) {
-  let maxSum = nums[0];
-  let startIdx = 0;
-  let endIdx = 0;
+// function maxSubArrayBruteForce(nums) {
+//   let maxSum = nums[0];
+//   let startIdx = 0;
+//   let endIdx = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    let currentSum = 0;
-    for (let j = i; j < nums.length; j++) {
-      currentSum = currentSum + nums[j];
-      if (currentSum > maxSum) {
-        maxSum = currentSum;
-        startIdx = i;
-        endIdx = j;
-      }
-    }
-  }
+//   for (let i = 0; i < nums.length; i++) {
+//     let currentSum = 0;
+//     for (let j = i; j < nums.length; j++) {
+//       currentSum = currentSum + nums[j];
+//       if (currentSum > maxSum) {
+//         maxSum = currentSum;
+//         startIdx = i;
+//         endIdx = j;
+//       }
+//     }
+//   }
 
-  return {
-    sum: maxSum,
-    subArray: nums.slice(startIdx, endIdx + 1),
-  };
-}
+//   return {
+//     sum: maxSum,
+//     subArray: nums.slice(startIdx, endIdx + 1),
+//   };
+// }
 
 // Time Complexity - O(n^2)
 // Space Complexity - O(1)
